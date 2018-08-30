5bbb2e8fc613ec6be7ef80e782192583338a0a73
Mohos Tamás <tomi@mohos.name>
2018-08-30 11:10:07 +0200

    chore(Env): env files moved to environment/config directory

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,4 @@ use Dotenv\Dotenv;
 
 require_once 'vendor/autoload.php';
 
-(new Dotenv(__DIR__ , '.env.' . getenv('PHP_ENV')))->load();
+(new Dotenv(__DIR__ . '/environment/config', '.env.' . getenv('PHP_ENV')))->load();
