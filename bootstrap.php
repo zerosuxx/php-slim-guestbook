d03c30196a3bd7fec0b1dc7bed89cfd76356a395
Mohos Tamás <tomi@mohos.name>
2018-08-30 13:44:25 +0200

    chore(Env): rename PHP_ENV to APPLICATION_ENV

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,4 @@ use Dotenv\Dotenv;
 
 require_once 'vendor/autoload.php';
 
-(new Dotenv(__DIR__ . '/environment/config', '.env.' . getenv('PHP_ENV')))->load();
+(new Dotenv(__DIR__ . '/environment/config', '.env.' . getenv('APPLICATION_ENV')))->load();
