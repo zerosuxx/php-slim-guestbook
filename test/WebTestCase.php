8666e5dab4d9199a116100898a1fbdd97f994213
Mohos Tamás <tomi@mohos.name>
2018-08-30 13:48:14 +0200

    docs(WebTestCase): remove header

--- test/WebTestCase.php
+++ test/WebTestCase.php
@@ -7,12 +7,6 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\Environment;
 
-/**
- * This is an example class that shows how you could set up a method that
- * runs the application. Note that it doesn't cover all use-cases and is
- * tuned to the specifics of this skeleton app, so if your needs are
- * different, you'll need to change it.
- */
 trait WebTestCase
 {
 
