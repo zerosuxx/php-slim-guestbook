c2a9009ef8dae2b4ac54f0f6192341949b0683c6
Mohos Tamás <tomi@mohos.name>
2018-09-03 11:39:14 +0200

    refactor(Guestbook): remove guestbook php template file

--- templates/guestbook.php
+++
@@ -1,6 +0,0 @@
-<table border="1px">
-    <tr><td>Name</td><td>Email</td><td>Message</td><td>Created At</td></tr>
-    <? foreach($messages as $message): ?>
-        <tr><td><?= $message['name'] ?></td><td><?= $message['email'] ?></td><td><?= $message['message'] ?></td><td><?= $message['created_at'] ?></td></tr>
-    <? endforeach; ?>
-</table>
