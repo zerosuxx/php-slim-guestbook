82159fa349ef309b0cc9b7cf06eab4955ec2ddae
Mohos Tamás <tomi@mohos.name>
2018-09-04 15:14:51 +0200

    fix(Database): init sql script allow all host

--- environment/init_sql/00-create-databases.sql
+++ environment/init_sql/00-create-databases.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS guestbook;
 CREATE DATABASE IF NOT EXISTS guestbook_test;
-CREATE USER 'guestbook'@'localhost' IDENTIFIED BY 'guestbook';
-GRANT ALL PRIVILEGES ON guestbook.* TO 'guestbook'@'localhost';
-GRANT ALL PRIVILEGES ON guestbook_test.* TO 'guestbook'@'localhost';
+CREATE USER 'guestbook'@'%' IDENTIFIED BY 'guestbook';
+GRANT ALL PRIVILEGES ON guestbook.* TO 'guestbook'@'%';
+GRANT ALL PRIVILEGES ON guestbook_test.* TO 'guestbook'@'%';
