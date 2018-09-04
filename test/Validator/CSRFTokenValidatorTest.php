6f6eb165d0f73154dc4346367ac9384f7349d0a8
Mohos Tamás <tomi@mohos.name>
2018-09-04 16:24:39 +0200

    test(CSRFTokenValidator): rename file

--- test/Validator/CSRFTokenValidatorTest.php
+++ test/Validator/CSRFTokenValidatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Test\Validator;
 
+use Guestbook\Validator\CSRFTokenValidator;
 use Guestbook\Validator\ValidationException;
 use PHPUnit\Framework\TestCase;
 
@@ -15,7 +16,7 @@ class CSRFTokenValidatorTest extends TestCase
      * @test
      */
     public function validate_GivenValidToken_ReturnsNullAndGenerateNewToken() {
-        $_SESSION['_csrf_token'] = 'valid-token';
+        $_SESSION[CSRFTokenValidator::TOKEN_KEY] = 'valid-token';
         $validator = new \Guestbook\Validator\CSRFTokenValidator();
         $this->assertNull($validator->validate('valid-token'));
         $this->assertNotEquals('valid-token', $validator->getToken());
@@ -26,7 +27,7 @@ class CSRFTokenValidatorTest extends TestCase
      */
     public function validate_GivenInValidToken_ThrowsException() {
         $this->expectException(ValidationException::class);
-        $_SESSION['_csrf_token'] = 'valid-token';
+        $_SESSION[CSRFTokenValidator::TOKEN_KEY] = 'valid-token';
         $validator = new \Guestbook\Validator\CSRFTokenValidator();
         $this->assertNull($validator->validate('in-valid-token'));
         $this->assertNotEquals('valid-token', $validator->getToken());
